777eb249779b7809f8534674054d2a721215a6e5
ThejanRupasinghe <thejanrupasinghe.14@cse.mrt.ac.lk>
2019-01-17 11:00:59 +0530

    Initial commit - backend structure

---
+++ backend/src/main/java/com/simplepos/backend/SimpleposBackendApplication.java
@@ -0,0 +1,14 @@
+package com.simplepos.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleposBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleposBackendApplication.class, args);
+    }
+
+}
+
