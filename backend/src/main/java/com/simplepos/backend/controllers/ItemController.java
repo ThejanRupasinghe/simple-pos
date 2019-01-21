e4aab5b41a08dae57f680af81389ab91b642c7f1
ThejanRupasinghe <thejanrupasinghe.14@cse.mrt.ac.lk>
2019-01-21 11:56:26 +0530

    refactor: minor changes

--- backend/src/main/java/com/simplepos/backend/controllers/ItemController.java
+++ backend/src/main/java/com/simplepos/backend/controllers/ItemController.java
@@ -1,4 +1,27 @@
 package com.simplepos.backend.controllers;
 
+import com.simplepos.backend.models.Item;
+import com.simplepos.backend.repository.ItemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "http://localhost:3000", maxAge = 3600, allowCredentials = "true")
+@RestController
+@RequestMapping("/api/item")
 public class ItemController {
+    private ItemRepository itemRepository;
+
+    @Autowired
+    public ItemController(ItemRepository itemRepository) {
+        this.itemRepository = itemRepository;
+    }
+
+    @GetMapping(path = "/all")
+    public Iterable<Item> getAllItems() {
+        return itemRepository.findAll();
+    }
+
 }
--- backend/src/main/java/com/simplepos/backend/repository/OrderRepository.java
+++ backend/src/main/java/com/simplepos/backend/repository/OrderRepository.java
@@ -5,11 +5,8 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.ArrayList;
-import java.util.Optional;
 
 @RepositoryRestResource(collectionResourceRel = "order")
 public interface OrderRepository extends MongoRepository<Order, String> {
-    Optional<ArrayList<Order>> findAllByStatus(String status);
-
-    // TODO: 1/20/19 check findAllByStatus
+    ArrayList<Order> findAllByStatus(String status);
 }
