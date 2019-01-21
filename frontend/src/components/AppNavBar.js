3fa08f769d4c576a9c4c0dd28f190e89d1673f8c
ThejanRupasinghe <thejanrupasinghe.14@cse.mrt.ac.lk>
2019-01-21 17:05:27 +0530

    feat: adds dependencies, nav bar

---
+++ frontend/src/components/AppNavBar.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    Container
+} from 'reactstrap';
+
+class AppNavBar extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isOpen: false
+        };
+    }
+
+    toggle = () => {
+        this.setState({
+            isOpen: !this.state.isOpen
+        });
+    };
+
+    render() {
+        return (
+            <div>
+                <Navbar color="dark" dark expand="sm" className="mb-5">
+                    <Container>
+                        <NavbarBrand href="/">SimplePOS</NavbarBrand>
+                        <NavbarToggler onClick={this.toggle}/>
+                        <Collapse isOpen={this.state.isOpen} navbar>
+                            <Nav className="ml-auto" navbar>
+                                {/*TODO - sign in or sign out*/}
+                                <NavItem>
+                                    <NavLink href="/signin">Sign In</NavLink>
+                                </NavItem>
+                            </Nav>
+                        </Collapse>
+                    </Container>
+                </Navbar>
+            </div>
+        );
+    }
+}
+
+export default AppNavBar;
